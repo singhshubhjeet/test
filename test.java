e3b4f5a4d19e072ac4b99c81fa6a0913105dc0bb
singhshubhjeet <29161835+singhshubhjeet@users.noreply.github.com>
2023-08-17 18:40:25 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,54 +1,42 @@
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@ConfigurationProperties(prefix = "proxy")
 public class RestTemplateConfig {
 
-    @Value("${proxy.username}")
-    private String proxyUsername;
-
-    @Value("${proxy.password}")
-    private String proxyPassword;
-
-    @Value("${api.bearerToken}")
-    private String apiBearerToken;
-
-    @Value("${proxy.host}")
-    private String proxyHost;
-
-    @Value("${proxy.port}")
-    private int proxyPort;
+    private String host;
+    private int port;
+    private String username;
+    private String password;
 
     @Bean
-    public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory());
-        
-        // Add Basic Authentication for the proxy
-        restTemplate.getInterceptors().add(new BasicAuthenticationInterceptor(proxyUsername, proxyPassword));
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setProxy(new java.net.Proxy(java.net.Proxy.Type.HTTP, new java.net.InetSocketAddress(host, port)));
+        builder = builder.requestFactory(() -> requestFactory);
 
-        return restTemplate;
+        return builder.basicAuthentication(username, password).build();
     }
 
-    private ClientHttpRequestFactory clientHttpRequestFactory() {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setProxy(createProxy());
-        return factory;
+    public void setHost(String host) {
+        this.host = host;
     }
 
-    private Proxy createProxy() {
-        return new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPort));
+    public void setPort(int port) {
+        this.port = port;
     }
 
-    // Method to create HttpHeaders with Bearer token
-    private HttpHeaders createHeadersWithBearerToken() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + apiBearerToken);
-        return headers;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
